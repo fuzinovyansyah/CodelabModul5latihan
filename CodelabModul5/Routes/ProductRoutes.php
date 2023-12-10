8440a99ee610ce103dee3022b90332b4e324d906
fuzinovyansyah <fuzi.novyansyah123@gmail.com>
2023-12-10 21:03:09 +0700

    init
    init

---
+++ CodelabModul5/Routes/ProductRoutes.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CodeLabModul5\Routes;
+
+include "Controllers/ProductController.php";
+
+use CodeLabModul5\Controllers\ProductController;
+
+class ProductRoutes
+{
+    public function handle($method,$path)
+    {
+        if ($method === 'GET' && $path === '/api/product'){
+            $controller = new ProductController();
+            echo $controller->index();
+        }
+        if ($method === 'GET' && strpos($path, '/api/product/') === 0){
+            $pathParts = explode('/',$path);
+            $id = $pathParts[count($pathParts)-1];
+
+            $controller = new ProductController();
+            echo $controller->getById($id);
+        }
+
+        if ($method==='POST' && $path === '/api/products'){
+            $controller = new ProductController();
+            echo $controller->insert();
+        }
+
+        if ($method === 'PUT' && strpos($path, '/api/product/') === 0){
+            $pathParts = explode('/',$path);
+            $id = $pathParts[count($pathParts)-1];
+
+            $controller = new ProductController();
+            echo $controller->update($id);
+        }
+
+        if ($method === 'DELETE' && strpos($path, '/api/product/') === 0){
+            $pathParts = explode('/',$path);
+            $id = $pathParts[count($pathParts)-1];
+
+            $controller = new ProductController();
+            echo $controller->delete($id);
+        }
+    }
+}
